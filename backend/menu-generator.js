cd2d3c202f5bfa479a41044e670ec4166c1163ff
PC_Kontrol_Room <sarani090500@gmail.com>
2025-07-06 10:07:06 +0800

    New Menu

--- backend/menu-generator.js
+++ backend/menu-generator.js
@@ -1,13 +1,5 @@
 function generate_dashboard(){
-    console.log ('panggil')
         const mainContainer = document.getElementById('main-container');
-//   if (!mainContainer) {
-//     console.error('main-container not found!');
-//     return;
-//   }
-//   else{
-//     console.log('terpanggil');
-//   }
         mainContainer.innerHTML = `
           <h1 id="main-title">DASHBOARD</h1>
           <div class="content-container fade-in" id="content-container">
@@ -44,5 +36,35 @@ function generate_dashboard(){
               </div>
             </div>
           </div>
+        `;
+}
+
+function generate_profile(){
+        const mainContainer = document.getElementById('main-container');
+        mainContainer.innerHTML = `
+          <h1 id="main-title">PROFILE</h1>
+        <div class="content-container fade-in" id="content-container">
+            <div class="profile-content">
+                <div class="profile-picture">
+                    <div class="picture">
+                        <img src="img/blank-profile-picture-973460_1280.png" alt="Profile">
+                        <a id="edit-profil-picture"><i class="bi bi-pencil-fill"></i></a>
+                    </div>
+                </div>
+                <div class="profile-data">
+                    <h1 id="profile-nama">Vinci Bla bla Bla</h1>
+                    <h3>ID : </h3>
+                    <h2>12345</h2>
+                    <h3>Jabatan/Divisi : </h3>
+                    <h2>ME</h2>
+                    <h3>Tanggal Lahir : </h3>
+                    <h2>80 Tahun</h2>
+                    <h3>No. Telpon : </h3>
+                    <h2>0852</h2>
+                    <h3>Email : </h3>
+                    <h2>blablabla@gmail.com</h2>
+                </div>
+            </div>
+        </div>
         `;
 }
